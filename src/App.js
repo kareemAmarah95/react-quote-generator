e34af61111210a0e4025d4b50ce361869d6dd988
Kareem Amara <kareemamarah2356@gmail.com>
2023-09-18 14:22:03 +0300

    fix quote genrator bugs and adding features

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
-import './App.css';
-import React, {useState} from 'react';
+import "./App.css";
+import React, { useState } from "react";
+import {
+  AiOutlineWhatsApp,
+  AiOutlineFacebook,
+  AiOutlineTwitter,
+} from "react-icons/ai";
 
+import { RiTelegramLine } from "react-icons/ri";
+import {
+  FacebookShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  TwitterShareButton,
+} from "react-share";
+import {
+  FacebookIcon,
+  TelegramIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "react-share";
 const App = () => {
   const url = "https://api.quotable.io/random";
   let quoteData = {
     content: "Let time be your only competitor.",
-    author: "Ahmed Saber"
-  }
-  const [quote, setQuote] = useState(quoteData)
+    author: "Ahmed Saber",
+  };
+  const [quote, setQuote] = useState(quoteData);
 
   const generateQuote = () => {
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setQuote(data)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        return setQuote(data);
       });
-  }
+  };
 
   const copy = () => {
-    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
-    alert('copied')
-  }
+    navigator.clipboard.writeText(
+      quote.author + " once said: " + quote.content
+    );
+    alert("copied");
+  };
 
   return (
     <>
@@ -30,13 +50,48 @@ const App = () => {
         <p>{quote.content}</p>
         <span>{quote.author}</span>
         <div className="btns">
-          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
           <button onClick={generateQuote}>Generate Another Quote</button>
+          <div className="parent-flex">
+            <div className="whatsapp">
+              <WhatsappShareButton url={`${quote.content} By ${quote.author}`}>
+                <WhatsappIcon logofillcolor="white" round={true} />
+              </WhatsappShareButton>
+            </div>
+            <div className="facebook">
+              <FacebookShareButton
+                url={`https://www.facebook.com/home.php`}
+                quote={`${quote.content} By ${quote.author}`}
+                hashtag={`#Quotes`}
+              >
+                <FacebookIcon logofillcolor="white" round={true} />
+              </FacebookShareButton>
+            </div>
+            <div className="telegram">
+              <TelegramShareButton
+                url={`https://web.telegram.org/a/#1987044822`}
+                title={`${quote.content} By ${quote.author}`}
+              >
+                <TelegramIcon logofillcolor="white" round={true} />
+              </TelegramShareButton>
+            </div>
+            <div className="twitter">
+              <TwitterShareButton
+                url="https://twitter.com/home"
+                // url={`${quote.content} By ${quote.author}`}
+                title={`${quote.content} By ${quote.author}`}
+                hashtag="Quotes"
+              >
+                <TwitterIcon logofillcolor="white" round={true} />
+              </TwitterShareButton>
+            </div>
+          </div>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
